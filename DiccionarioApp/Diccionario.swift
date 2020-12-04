0133c27a5e72e6397fc01fcc16625266187b48a8
esTechVMG <vicente.maroto@escuelaestech.es>
2020-12-04 15:10:14 +0100

    Add init to Diccionario class

--- DiccionarioApp/Diccionario.swift
+++ DiccionarioApp/Diccionario.swift
@@ -1,18 +1,22 @@
 import Foundation
 
 class Diccionario{
-    var wordList:Array<(String,String)>=[
-        ("payaso","clown"),
-        ("gratis","free"),
-        ("capa","layer"),
-        ("perro","dog"),
-        ("cangrejo","crab"),
-        ("realidad","reality")
-    ]
-    func searchTranslation(word:String) -> [(String,String)] {
+    private var wordList:Array<(String,String)>
+    
+    init(){
+        wordList=[
+            ("payaso","clown"),
+            ("gratis","free"),
+            ("capa","layer"),
+            ("perro","dog"),
+            ("cangrejo","crab"),
+            ("realidad","reality")
+        ]
+    }
+    public func searchTranslation(word:String) -> [(String,String)] {
         return wordList.filter({(wordItem:(es:String,en:String)) in return wordItem.0.contains(word) || wordItem.1.contains(word)})
     }
-    func addToWordList(word:(spanish:String, english:String)) -> Bool {
+    public func addToWordList(word:(spanish:String, english:String)) -> Bool {
         if checkEmpty(word: word){
             return false
         }else{
@@ -20,7 +24,7 @@ class Diccionario{
             return true
         }
     }
-    func deleteWord(word:String) -> Bool {
+    public func deleteWord(word:String) -> Bool {
         for index in wordList.startIndex ... wordList.endIndex-1 {
             if wordList[index].0 == word || wordList[index].1 == word {
                 wordList.remove(at: index)
@@ -29,17 +33,17 @@ class Diccionario{
         }
         return false
     }
-    func modifyWord(pos:Int,newModify:(String,String)) -> Void {
+    public func modifyWord(pos:Int,newModify:(String,String)) -> Void {
         if(checkEmpty(word: newModify)){
             wordList[pos] = newModify
         }
     }
     //Added just for easing app development. not sure if its really going to be used
-    func getWordAtIndex(pos:Int) -> (String,String) {
+    public func getWordAtIndex(pos:Int) -> (String,String) {
         return wordList[pos]
     }
     
-    func checkEmpty(word:(String,String)) -> Bool {
+    public func checkEmpty(word:(String,String)) -> Bool {
         return !word.0.isEmpty || word.1.isEmpty
     }
 }
